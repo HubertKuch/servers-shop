a47d870e9b589981a71fff2ef1c15b99dc157c4f
hubert <kuchhubert@gmail.com>
2022-05-02 14:12:39 +0200

    Add product repository

--- Repositories.php
+++ Repositories.php
@@ -3,15 +3,18 @@
 namespace Servers;
 
 use Servers\Models\Payment;
+use Servers\Models\Product;
 use Servers\Models\User;
 use Avocado\ORM\AvocadoRepository;
 
 class Repositories {
     public static AvocadoRepository $userRepository;
     public static AvocadoRepository $paymentsRepository;
+    public static AvocadoRepository $productsRepository;
 
     public static function init(): void {
         self::$userRepository = new AvocadoRepository(User::class);
         self::$paymentsRepository = new AvocadoRepository(Payment::class);
+        self::$productsRepository = new AvocadoRepository(Product::class);
     }
 }
