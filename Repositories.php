d0910d66c8874456c842f996ae81fb4fd882e3bf
hubert <kuchhubert@gmail.com>
2022-05-02 20:53:46 +0200

    Add logs repository

--- Repositories.php
+++ Repositories.php
@@ -6,15 +6,18 @@ use Servers\Models\Payment;
 use Servers\Models\Product;
 use Servers\Models\User;
 use Avocado\ORM\AvocadoRepository;
+use Servers\Models\Log;
 
 class Repositories {
     public static AvocadoRepository $userRepository;
     public static AvocadoRepository $paymentsRepository;
     public static AvocadoRepository $productsRepository;
+    public static AvocadoRepository $logsRepository;
 
     public static function init(): void {
         self::$userRepository = new AvocadoRepository(User::class);
         self::$paymentsRepository = new AvocadoRepository(Payment::class);
         self::$productsRepository = new AvocadoRepository(Product::class);
+        self::$logsRepository = new AvocadoRepository(Log::class);
     }
 }
