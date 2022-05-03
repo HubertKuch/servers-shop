e1bd3ea6018c9eb4082ec022f912010c5ec48a40
hubert <kuchhubert@gmail.com>
2022-05-03 08:54:59 +0200

    Make register view

--- views/register.php
+++ views/register.php
@@ -1,12 +1,29 @@
-<html>
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Logowanie</title>
+    <base href="http://localhost/servers/">
+    <link rel="stylesheet" href="style/main.css">
 </head>
 <body>
-    register
+<form action="index.php/api/login" method="post" class="login__form">
+    <p>Rejestracja</p>
+    <label>
+        <input type="text" name="username" class="form__input" placeholder="username">
+    </label>
+    <label>
+        <input type="text" name="email" class="form__input" placeholder="email">
+    </label>
+    <label>
+        <input type="password" name="password" class="form__input form__input--password" placeholder="password">
+    </label>
+    <button type="submit" class="form__button">Zarejestruj się</button>
+    <div class="form__bottom-section">
+        <a href="views/login.php">Logowanie</a>
+    </div>
+</form>
 </body>
 </html>
