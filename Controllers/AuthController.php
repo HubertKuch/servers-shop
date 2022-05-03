a57499816c34f048e224c34f20465cb287774353
hubert <kuchhubert@gmail.com>
2022-05-03 08:06:23 +0200

    Add auth controller

--- Controllers/AuthController.php
+++ Controllers/AuthController.php
@@ -21,4 +21,8 @@ class AuthController {
             redirectToLoginViewWithMessage($messages);
         }
     }
+
+    public static function redirectToLoginWithMessage(array $error) {
+        redirectToLoginViewWithMessage($error);
+    }
 }
