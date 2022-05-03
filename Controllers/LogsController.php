af8e7a059e5ff57bd88092f5a6f502eb9afbf39b
hubert <kuchhubert@gmail.com>
2022-05-03 08:06:14 +0200

    Add logs controller

---
+++ Controllers/LogsController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Servers\Controllers;
+
+use Servers\Models\LogType;
+use Servers\Utils\LogsTemplates;
+use Servers\Models\Log;
+use Servers\Repositories;
+
+class LogsController {
+    public static final function saveUserLoginLog(int $userId, string $username): void {
+        $message = LogsTemplates::userLogin($userId, $username);
+        $log = new Log(LogType::AUTH->value, $userId, null, null, time(), $message);
+
+        Repositories::$logsRepository->save($log);
+    }
+
+    public static final function saveUserRegisterLog(int $id): void {
+        $message = LogsTemplates::userRegister($id);
+        $log = new Log(LogType::AUTH->value, $id, null, null, time(), $message);
+
+        Repositories::$logsRepository->save($log);
+    }
+
+    public static final function savePaymentLog(int $id): void {
+        $message = LogsTemplates::payment($id);
+        $log = new Log(LogType::PAYMENT->value, null, null, $id, time(), $message);
+
+        Repositories::$logsRepository->save($log);
+    }
+
+    public static final function saveNewProductLog(int $id): void {
+        $message = LogsTemplates::newProduct($id);
+        $log = new Log(LogType::PRODUCT->value, null, $id, null, time(), $message);
+
+        Repositories::$logsRepository->save($log);
+    }
+
+    public static final function saveProductSold(int $id): void {
+        $message = LogsTemplates::productSold($id);
+        $log = new Log(LogType::PRODUCT->value, null, $id, null, time(), $message);
+
+        Repositories::$logsRepository->save($log);
+    }
+}
---
+++ Utils/LogsTemplates.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Servers\Utils;
+
+use phpDocumentor\Reflection\Utils;
+
+class LogsTemplates {
+    public static function userLogin(int $id, string $username): string {
+        return "User with $id and $username was logged in.";
+    }
+
+    public static function userRegister(int $id): string {
+        return "New user: username: id - $id";
+    }
+
+    public static function payment(int $id): string {
+        return "New payment: id - $id";
+    }
+
+    public static function productSold(int $id): string {
+        return "Server id - $id was sold";
+    }
+
+    public static function newProduct(int $id): string {
+        return "New server with id - $id";
+    }
+}
