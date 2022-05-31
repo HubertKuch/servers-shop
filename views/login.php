70be366f5799872fdae6ec4f097f531f385f6a29
witekmikolajczak <witekmikolajczak@gmail.com>
2022-05-31 18:10:08 +0200

    changing the appearance of the login page

--- views/login.php
+++ views/login.php
@@ -11,26 +11,64 @@ use Servers\Utils\Environment;
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Logowanie</title>
     <base href="<?= Environment::getBaseURL() ?>">
-    <link rel="stylesheet" href="style/main.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="style/app.min.css" rel="stylesheet">
+
 </head>
-<body>
-    <form action="index.php/api/login" method="post" class="login__form">
-        <div class="form__error-container">
-            <?php
-                foreach ($errors as $error) echo "<span class='error-container__error'>$error</span>";
-            ?>
+<body class="bg-gradient-primary">
+<!--class="login__form"-->
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <div class="row">
+                        <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                        <div class="col-lg-6">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <h1 class="h4 text-gray-900 mb-4">Zaloguj się!</h1>
+                                </div>
+                                <form class="user" action="index.php/api/login" method="post" >
+                                    <div class="form-group mb-3">
+                                        <input type="text" class="form-control form-control-user" id="exampleInputEmail" name="username" aria-describedby="emailHelp" placeholder="Podaj login..." required>
+                                    </div>
+                                    <div class="form-group mb-5">
+                                        <input type="password" class="form-control form-control-user" name="password" id="exampleInputPassword" placeholder="Podaj hasło" required>
+                                    </div>
+
+                                    <button type="submit" class="btn btn-primary btn-user btn-block mt-auto mt-5">
+                                        Zaloguj się
+                                    </button>
+
+                                    <hr>
+                                </form>
+<!--                                <div class="text-center">-->
+<!--                                    <a class="small" href="forgot-password.html">Forgot Password?</a>-->
+<!--                                </div>-->
+                                <div class="text-center">
+                                    <a class="small" href="index.php/register">Nie masz konta? Załóż je!</a>
+                                </div>
+                                <hr>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <p>Logowanie</p>
-        <label>
-            <input type="text" name="username" class="form__input" placeholder="username">
-        </label>
-        <label>
-            <input type="password" name="password" class="form__input form__input--password" placeholder="password">
-        </label>
-        <button type="submit" class="form__button">Zaloguj się</button>
-        <div class="form__bottom-section">
-            <a href="index.php/register">Nie masz konta? Zaloz je</a>
-        </div>
-    </form>
+    </div>
+</div>
+
+<!-- Bootstrap core JavaScript-->
+<script src="js/jquery/jquery.min.js"></script>
+<script src="js/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="js/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="js/app/app.min.js"></script>
 </body>
 </html>
