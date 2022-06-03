72edc239c3c9db8c6748cad5d8b4b93700905d2f
witekmikolajczak <witekmikolajczak@gmail.com>
2022-06-03 22:06:43 +0200

    change "create server" looks
    - add toggle button to sidebar nav
    - change form structure in "create server" page

--- views/components/Servers.php
+++ views/components/Servers.php
@@ -5,19 +5,21 @@ namespace Servers\views\components;
 class Servers {
     public static final function minecraftEgg(string $imgSource, string $name, int $eggId): void {
         printf('
-            <div class="egg" data-egg-name="%s" data-egg-id="%s">
+            <div class="egg row" data-egg-name="%s" data-egg-id="%s">
                 <img src="%s" alt="%s" class="egg__img">
-                <div class="egg__name">%s</div>
+                <div class="egg__name d-flex align-items-center justify-content-center ml-3">
+                    <h1 class="text-gray-900">%s</h1>    
+                </div>
             </div>
         ', $name, $eggId, $imgSource, $name, $name);
     }
 
     public static final function package(int $packageId, string $packageImgSource, string $name, string $desc, float $price): void {
         printf('
-            <div class="package" data-package-id="%s" data-package-price="%s">
+            <div class="package row" data-package-id="%s" data-package-price="%s">
                 <img src="%s" alt="%s" class="egg__img">
-                <div class="egg__name">%s</div>
-                <div class="egg__desc">%s</div>
+                <div class="egg__name d-flex align-items-center justify-content-center ml-3">%s</div>
+                <div class="egg__desc d-flex align-items-center justify-content-center ml-3">%s</div>
             </div>
         ', $packageId, $price, $packageImgSource, $name, $name, $desc);
     }
