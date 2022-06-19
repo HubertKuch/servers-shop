0ff6f2615f846e7f777fa0543e7b488a84e2ef0e
hubert <kuchhubert@gmail.com>
2022-06-19 22:56:58 +0200

    Fix bug with getting `ENVIRONMENT` variable in `getBaseURL`

--- Utils/Environment.php
+++ Utils/Environment.php
@@ -4,6 +4,6 @@ namespace Servers\Utils;
 
 class Environment {
     public static function getBaseURL(): string {
-        return getenv("ENVIRONMENT") === "PRODUCTION" ? "http://{$_SERVER['HTTP_HOST']}/" : "http://{$_SERVER['HTTP_HOST']}/srv/";
+        return $_ENV['ENVIRONMENT'] === "PRODUCTION" ? "http://{$_SERVER['HTTP_HOST']}/" : "http://{$_SERVER['HTTP_HOST']}/srv/";
     }
 }
