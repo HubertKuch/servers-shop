5dfb39ddc885778d92290fa1ba7273dd69d4b218
hubert <kuchhubert@gmail.com>
2022-06-20 12:55:12 +0200

    Create `imports.sql`

---
+++ database/imports.sql
@@ -0,0 +1,5 @@
+INSERT INTO
+    `package` (`id`, `name`, `ram_size`, `disk_size`, `processor_power`, `cost`, `image_src`)
+VALUES
+      (1, 'Standard', 1024, 2048, 100, 25, 'views/assets/mc_server_1.jpg'),
+      (2, 'Premium', 2048, 8192, 200, 50, 'views/assets/mc_server_2.png');
